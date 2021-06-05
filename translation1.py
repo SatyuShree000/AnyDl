0ca8a75a552f7669a10cdeae67ec6a88bbd1fb14
Satyu <70142319+SatyuShree000@users.noreply.github.com>
2021-06-05 20:12:28 +0530

    Create translation1.py

---
+++ translation1.py
@@ -0,0 +1,30 @@
+class Translation1(object):
+  
+  ##############################################################################################################################
+##############################################################################################################################
+
+
+#DON'T CHANGE THE NUMBERS IN THE FLOWER BRACKETS AND THE ORDER OF PERCENTAGE, DONE, TOTAL, SPEED, ETA ONLY CHANGE THE THEME 
+
+      PROGRESS = """
+Percentage : {0}%
+Done ✅: {1}
+Total 🌀: {2}
+Speed 🚀: {3}/s
+ETA 🕰: {4}
+"""
+       
+      DOWNLOAD_PROGRESS = "▪️"
+      UPLOAD_PROGRESS = "▫️"
+
+##############################################################################################################################
+##############################################################################################################################
+
+      DOWNLOAD_START = "Trying to Download 📥"
+      DOWNLOAD_COMPLETE = "✅ Media Downloaded successfully\nPreparing for upload"
+      UPLOAD_START = "Trying to Upload 📤"
+      UPLOAD_COMPLETE = "THANKS FOR USING ME \n Sponsered By :- @All_Movie_Rockers"
+      SAVED_CUSTOM_THUMB_NAIL = "✅ Saved Thumbnail Successfully. This will be deleted in 24hrs"
+      BANNED_TEXT = "YOU ARE BANNED. SO YOUR ARE NOT ABLE TO USE ME 🐒"
+      REPLY_TEXT = "👩‍✈️ Reply to the media which you need to convert"
+      DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Deleted Successfully ✅"
